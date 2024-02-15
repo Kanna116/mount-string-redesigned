a687258087209372ee0e67fad59beaecd4e5892f
Kanna116 <muralikanna77@gmail.com>
2024-02-15 12:01:21 +0530

    hero section created and made responsive also

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import HeroSection from '../components/herosection'
 
 const HomePage = () => {
   return (
     <>
         <Navbar />
+        <HeroSection />
     </>
   )
 }
