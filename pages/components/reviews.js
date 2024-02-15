83bfaaf5603d410b144e96cdff39cb804623b4a0
Kanna116 <muralikanna77@gmail.com>
2024-02-15 18:57:48 +0530

    basic data implemented and checked

--- pages/components/reviews.js
+++ pages/components/reviews.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { reviews } from '../constants'
+import Image from 'next/image'
+import { FaStar } from "react-icons/fa";
 
 const Reviews = () => {
-  return (
-    <div className='w-full h-[80vh] px-[24px] md:px-[64px] lg:px-[100px] py-10'>
-        <h1 className='text-center text-3xl font-semibold'>Here what our People Say</h1>
-        <div className='w-full mt-5 h-full bg-red-400'>
-            <div>
-                
+    return (
+        <div className='w-full h-[80vh] px-[24px] md:px-[64px] lg:px-[100px] py-10'>
+            <h1 className='text-center text-3xl font-semibold'>Here what our People Say</h1>
+            <div className='w-full mt-5 h-full bg-red-400'>
+                {reviews.map((item, index) => {
+                    const { name, date, review, image,rating } = item
+
+
+
+                    return (<div className='w-full h-fit flex gap-3'>
+                        <div className="profile ">
+                            <Image
+                                src={image}
+                                width={500}
+                                height={500}
+                                alt='reviewer image'
+                                draggable={false}
+                            />
+                        </div>
+                        <div className='flex flex-col items-start justify-center gap-2'>
+                            <h1>{name}</h1>
+                            <h4>{rating}</h4>
+                            <p>{review}</p>
+                            <h4>{date}</h4>
+                        </div>
+                    </div>)
+                })}
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Reviews
--- pages/constants/index.js
+++ pages/constants/index.js
@@ -73,6 +73,7 @@ export const reviews=[
         date:'6/1/2024',
         review:'Wonderful place to work.. The team is fabulous',
         rating:'5',
+        image:'/images/reviewer.webp'
     },
     {
         name:'Akki Akshay4B8',
@@ -80,18 +81,21 @@ export const reviews=[
         review:`Feeling happy to working in Mount string technology, good environment for freshers to improve their skills and knowledge.
         Overall it's beter opportunity to enhance our skills from a scratch.`,
         rating:'5',
+        image:'/images/reviewer.webp'
     },
     {
         name:'Pandu Babu',
         date:'17/11/2023',
         review:'" Threads Of Future Technology " This Tag line is really apt For this Company. And coming to Training Process Teaching Proficiency is So Effective For Career Growth .',
         rating:'5',
+        image:'/images/reviewer.webp'
     },
     {
         name:'Dileep Varma Gadhiraju ',
         date:'6/1/2024',
         review:'Wonderful place to work.. The team is fabulous',
         rating:'5',
+        image:'/images/reviewer.webp'
     },
     {
         name:'Akki Akshay4B8',
@@ -99,12 +103,14 @@ export const reviews=[
         review:`Feeling happy to working in Mount string technology, good environment for freshers to improve their skills and knowledge.
         Overall it's beter opportunity to enhance our skills from a scratch.`,
         rating:'5',
+        image:'/images/reviewer.webp'
     },
     {
         name:'Pandu Babu',
         date:'17/11/2023',
         review:'" Threads Of Future Technology " This Tag line is really apt For this Company. And coming to Training Process Teaching Proficiency is So Effective For Career Growth .',
         rating:'5',
+        image:'/images/reviewer.webp'
     },
     
 ]
