4c01c3f0d846a232ccec4a0fd6fb13f92c5f6481
Kanna116 <muralikanna77@gmail.com>
2024-02-15 18:41:14 +0530

    data of reviews added to constants

--- pages/components/reviews.js
+++ pages/components/reviews.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+    <div className='w-full h-[80vh] px-[24px] md:px-[64px] lg:px-[100px] py-10'>
+        <h1 className='text-center text-3xl font-semibold'>Here what our People Say</h1>
+        <div className='w-full mt-5 h-full bg-red-400'>
+            <div>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
