7d14a03dda710478f50416e24d7a78eb852a3c9f
Kanna116 <muralikanna77@gmail.com>
2024-02-16 15:24:44 +0530

    logo added to footer

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,8 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full h-[70vh] shadow-[0px_-10px_20px_#00000005] flex items-start justify-center px-24 lg:px[64px] md:px-[64px]'>
+        <div className="company-details w-1/3 h-full flex flex-col items-start justify-start gap-5">
+            <div className="logo w-[150px] p-5 aspect-square ">
+                <Image
+                src={'/images/mount-string-logo.webp'}
+                width={500}
+                height={500}
+                alt='company logo'
+                draggable={false}
+                className='w-full h-full object-contain select-none' />
+            </div>
+
+        </div>
+
+        <h1></h1>
+        <div className="links w-2/3 h-full bg-red-100"></div>
+
+    </div>
   )
 }
 
