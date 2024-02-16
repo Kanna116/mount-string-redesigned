1daac42e349d8990c8f81d26c96a38e0b1598e3a
Kanna116 <muralikanna77@gmail.com>
2024-02-16 15:56:01 +0530

    copy right added with auto uear changing feature

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -2,13 +2,18 @@ import Image from 'next/image';
 import { FaFacebook, FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { companyDetails, footerLinks } from '../constants';
+import { FaRegCopyright } from "react-icons/fa6";
 
 const Footer = () => {
 
-    const { name, address, logo, phone, mail } = companyDetails
+    const { name, address, logo, phone, mail } = companyDetails;
+
+    //to make a constant changing year in copyrights
+    const date = new Date();
+    const currentYear = date.getFullYear();
 
     return (
-        <div className='w-full h-[70vh] shadow-[0px_-10px_20px_#00000005] flex items-start justify-center px-24 lg:px[64px] md:px-[64px]'>
+        <div className='w-full h-[70vh] relative shadow-[0px_-10px_20px_#00000005] flex items-start justify-center px-24 lg:px[64px] md:px-[64px]'>
             {/* company details */}
             <div className="company-details w-1/3 h-full flex flex-col items-start justify-start gap-5">
                 <div className="logo w-[150px] p-5 aspect-square">
@@ -21,7 +26,7 @@ const Footer = () => {
                         className='w-full h-full object-cover select-none' />
                 </div>
 
-                
+
                 <h1 className='text-lg font-semibold'>{name}</h1>
                 <p className='text-xs w-2/3'>{address}</p>
                 <div className='mt-7'>
@@ -58,7 +63,7 @@ const Footer = () => {
 
 
             {/* copyright */}
-            <h3></h3>
+            <h3 className='absolute bottom-10 left-10 flex items-center gap-2'>Copyright <span><FaRegCopyright /></span> {currentYear} Mount String Technologies</h3>
 
         </div>
     )
