29c4367ae520858b24383f5de399b61a501e70d3
Kanna116 <muralikanna77@gmail.com>
2024-02-16 14:30:56 +0530

    button changed in quick scheduling

--- pages/components/quickscheduling.js
+++ pages/components/quickscheduling.js
@@ -5,7 +5,7 @@ const QuickScheduling = () => {
     return (
         <div className='w-full lg:h-[40vh] h-fit flex  flex-col items-center justify-center lg:px-[100px] md:px-[64px] px-[24px] my-5 py-10 gap-7'>
             <h1 className='text-3xl lg:w-3/4 w-full font-semibold leading-none text-center'>{quickscheduling.text}</h1>
-            <button className='px-5 py-2 bg-black  text-white text-xl rounded-full'>Contact US</button>
+            <button className='px-10 py-2 bg-black  text-white text-xl rounded-full'>Schedule a Quick Meeting</button>
         </div>
     )
 }
