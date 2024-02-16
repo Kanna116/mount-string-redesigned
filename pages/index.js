03124d2f9dec51d6381a8978200cb6c407eb1131
Kanna116 <muralikanna77@gmail.com>
2024-02-16 18:36:03 +0530

    font issue removed

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ const Home = () => {
     <>
     <Head>
       <title>Mount Strings</title>
+      <link href="https://api.fontshare.com/v2/css?f[]=montserrat@500,1,900,700,701,801,200,401,301,400,601,100,101,2,201,800,300,600,901,501&display=swap" rel="stylesheet"></link>
     </Head>
 
     <HomePage />
