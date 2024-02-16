e61aa08b0ee63c21830cf0e4e7fca15b53267816
Kanna116 <muralikanna77@gmail.com>
2024-02-16 14:29:18 +0530

    a constant is made and implemented

--- pages/components/quickscheduling.js
+++ pages/components/quickscheduling.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import { quickscheduling } from '../constants'
 
 const QuickScheduling = () => {
     return (
         <div className='w-full lg:h-[40vh] h-fit flex  flex-col items-center justify-center lg:px-[100px] md:px-[64px] px-[24px] my-5 py-10 gap-7'>
-            <h1 className='text-3xl lg:w-3/4 w-full font-semibold leading-none text-center'>We value your business and you time . so let’s get on to a meeting . 
-And Start the most wonderful projects of your choice</h1>
+            <h1 className='text-3xl lg:w-3/4 w-full font-semibold leading-none text-center'>{quickscheduling.text}</h1>
             <button className='px-5 py-2 bg-black  text-white text-xl rounded-full'>Contact US</button>
         </div>
     )
