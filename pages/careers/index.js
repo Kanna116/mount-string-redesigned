a594cfe1f0496b81f4d43ac6a10e8e3b6f6131c5
Kanna116 <muralikanna77@gmail.com>
2024-02-18 18:56:42 +0530

    Update index.js

--- pages/careers/index.js
+++ pages/careers/index.js
@@ -17,7 +17,6 @@ const CareersPage = () => {
       <CareerBanner />
       <div className='w-full h-fit lg:py-32 py-12 px-[24px] md:px-[64px] lg:px-[100px] flex lg:flex-row  gap-7 lg:gap-0 flex-col items-start justify-evenly'>
         <ApplyNow />
-
         <CurrentOpenings />
       </div>
 
