d8b17b7fa8a7dd5b4a0ced56993735231628eb71
Kanna116 <muralikanna77@gmail.com>
2024-02-18 19:08:17 +0530

    small issue fixed

--- pages/components/careerbanner.js
+++ pages/components/careerbanner.js
@@ -8,7 +8,7 @@ const CareerBanner = () => {
 
                 <h1 className='lg:text-5xl text-4xl font-bold'>You know what.<br /> We are hiring</h1>
                 <h3 className='text-2xl font-semibold mt-5'>Join Our Team</h3>
-                <p className='w-3/4'>If you're interested in one of our open positions, start by applying here and attaching your resume.</p>
+                <p className='w-3/4'>If you are interested in one of our open positions, start by applying here and attaching your resume.</p>
             </div>
             <div className='lg:h-[40vh] h-[20vh] mt-10 lg:mt-0 aspect-square '>
                 <Image
